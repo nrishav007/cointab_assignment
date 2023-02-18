38c10295b22f3dd49ab1e78def83494e47ee48b7
Rishav Neogi <nrishav31@gmail.com>
2023-02-18 23:48:10 +0530

    post and get method added in user routes

--- Backend/index.js
+++ Backend/index.js
@@ -2,29 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const env = require("dotenv");
-const  mdbConnection  = require("./Configs/DB");
+const { test, listen } = require("./Controllers/AllFuction");
+const userRoute = require("./Routes/User.route");
+let port = process.env.PORT || 4000;
 app.use(express.json());
 env.config();
-let port = process.env.PORT || 4000;
 app.use(
   cors({
     origin: "*",
   })
 );
+app.use("/user",userRoute);
+app.get("/", test);
 
-app.get("/", async(req, res) => {
-  try {
-    res.send({ msg: "deployed" });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.listen(port, () => {
-  try {
-    mdbConnection;
-    console.log(`listening to http://localhost:${port}`);
-  } catch (error) {
-    console.log(error);
-  }
-});
+app.listen(port, listen);
