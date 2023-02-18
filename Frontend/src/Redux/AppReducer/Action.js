d922f66fdfacb2bae756f22ad6504d82e1348569
Rishav Neogi <nrishav31@gmail.com>
2023-02-19 04:13:21 +0530

    redux added in frontend

---
+++ Frontend/src/Redux/AppReducer/Action.js
@@ -0,0 +1,8 @@
+import * as types from "./ActionTypes"
+
+export const getReq = () =>{
+    return {
+        type : types.GET_REQUEST
+    }
+}
+
---
+++ Frontend/src/Redux/AppReducer/Reducer.js
@@ -0,0 +1,91 @@
+import * as types from "./ActionTypes"
+const InitialData = {
+    userData :[],
+    isLoading : false,
+    isError : false
+}
+
+export default function Reducer (state=InitialData, action) {
+    const {payload,type} = action;
+    switch(type){
+        case types.GET_REQUEST : {
+            return {
+                ...state,
+                isLoading : true,
+                isError : false
+            }
+        }
+
+        case types.GET_SUCCESS : {
+            return {
+                ...state,
+                isLoading : false,
+                userData : payload,
+                isError : false
+            }
+        }
+
+        case types.GET_FAILURE : {
+            return {
+                ...state,
+                isLoading : false,
+                userData:[],
+                isError : true
+            }
+        }
+
+        case types.POST_REQUEST : {
+            return {
+                ...state,
+                isLoading : true,
+                isError : false
+            }
+        }
+
+        case types.POST_SUCCESS : {
+            return {
+                ...state,
+                isLoading : false,
+                isError : false
+            }
+        }
+
+        case types.POST_FAILURE : {
+            return {
+                ...state,
+                isLoading : false,
+                userData:[],
+                isError : true
+            }
+        }
+        
+        case types.DELETE_REQUEST : {
+            return {
+                ...state,
+                isLoading : true,
+                isError : false
+            }
+        }
+
+        case types.DELETE_SUCCESS : {
+            return {
+                ...state,
+                isLoading : false,
+                userData:[],
+                isError : false
+            }
+        }
+
+        case types.DELETE_FAILURE : {
+            return {
+                ...state,
+                isLoading : false,
+                userData:[],
+                isError : true
+            }
+        }
+        default :
+        return state
+    }
+    
+}
